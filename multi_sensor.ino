381c48033a0a7c3b5d7def3f2e052d4627de2fb7
Aleksandr Zhuikov <aleksandr@transferwise.com>
2016-12-27 15:21:00 +0200

    introducing DEOL resitors

--- multi_sensor.ino
+++ multi_sensor.ino
@@ -1,7 +1,18 @@
-int ledPin = 13;      // select the pin for the LED
+const char SHORT_CISRCUIT = 'S';
+const char SECURE = 'Y';
+const char ALARM = 'A';
+const char CUT = 'C';
+const char NOT_KNOWN = 'U';
+const char HEALTH_CHECK = 'H';
+
+const int ledPin = 13;      // select the pin for the LED
+
 int sensorPins[] = {A0, A1};
 int sensorValue[] = {0, 0};  // variable to store the value coming from the sensor
-int threshold[] = {50, 50};
+int alarmThreshold[] = {150, 150};
+int cutThreshold[] = {900, 900};
+int secureThreshold[] = {70, 70};
+char sensorState[] = {NOT_KNOWN, NOT_KNOWN};
 int coolDownMillis = 500;
 unsigned long alarmCooldownStart[2];
 boolean alarmRaised[2];
@@ -10,40 +21,62 @@ unsigned long heartbearStart;
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
+
+  for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++) { 
+    initSensorPin(i);
+  }
+
   Serial.begin(9600);
   Serial.flush();
 }
 
 void loop() {
-  for (int i = 0; i<2; i++) { 
+  for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++) { 
     checkSensor(i);
     maybeSendHeartBeat();
     delay(5);
   }
 }
 
+void initSensorPin(int sensor) {
+  pinMode(sensorPins[sensor], INPUT);
+  digitalWrite(sensorPins[sensor], HIGH);  // set pullup on analog pin 0 
+}
+
 void checkSensor(int sensor) {
   sensorValue[sensor] = analogRead(sensorPins[sensor]);
-  //Serial.println(sensorValue[sensor]);
+//  Serial.println(sensorValue[sensor]);
 
-  if (sensorValue[sensor] > threshold[sensor]) {
+  if (sensorValue[sensor] > cutThreshold[sensor]) {
+    sensorState[sensor] = CUT;
+    maybeRaiseAlarm(CUT, sensor);
+  } else if (sensorValue[sensor] > alarmThreshold[sensor] && sensorValue[sensor] <= cutThreshold[sensor]) {
     alarmCooldownStart[sensor] = millis();  
-    if (!alarmRaised[sensor]) {
-      alarmRaised[sensor] = true;
-      String msg = String("N0") + sensor;
-      Serial.println(msg);
-      digitalWrite(ledPin, HIGH);  
-    } 
-  } else {
+    sensorState[sensor] = ALARM;
+    maybeRaiseAlarm(ALARM, sensor);
+  } else if (sensorValue[sensor] > secureThreshold[sensor] && sensorValue[sensor] <= alarmThreshold[sensor]) {
     if (millis() - alarmCooldownStart[sensor] >= coolDownMillis && alarmRaised[sensor]) {
       alarmRaised[sensor] = false;
+      sensorState[sensor] = SECURE;
       if (!hasRaisedAlarms()) {
         digitalWrite(ledPin, LOW); 
       }
-      String msg = String("Y0") + sensor;
+      String msg = String(SECURE) + sensor;
       Serial.println(msg);        
     }
+  } else {
+    sensorState[sensor] = SHORT_CISRCUIT;
+    maybeRaiseAlarm(SHORT_CISRCUIT, sensor);
   }
+}
+
+void maybeRaiseAlarm(char alarmType, int sensor) {
+  if (!alarmRaised[sensor]) {
+    alarmRaised[sensor] = true;
+    String msg = String(alarmType) + sensor;
+    Serial.println(msg);
+    digitalWrite(ledPin, HIGH);
+  }    
 }
 
 void maybeSendHeartBeat() {
@@ -54,17 +87,17 @@ void maybeSendHeartBeat() {
 }
 
 void sendHeartBeat() {
-  String msg = String("H0");
-  if (hasRaisedAlarms()) {
-    msg += "Y";
-  } else {
-    msg += "N";
+  String msg = String(HEALTH_CHECK);
+
+  for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++) { 
+    msg += sensorState[i];
   }
+
   Serial.println(msg);
 }
 
 boolean hasRaisedAlarms() {
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < (sizeof(sensorPins)/sizeof(int)); i++) {
     if (alarmRaised[i] == true) {
       return true;
     }
